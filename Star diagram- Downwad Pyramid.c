e2220e1551eec9cf33a5c5092f81fe619e0f28e8
Azan1100 <106399369+Azan1100@users.noreply.github.com>
2022-09-22 09:08:15 +0530

    Add files via upload

---
+++ Star diagram- Downwad Pyramid.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+ {
+    int i, j;
+    for(i=1;i<=6;i++)
+    {
+        for(j=6;j>=i;j--)
+        {
+            printf("*\t");
+        }
+         printf("\n");
+    }
+
+
+    return 0;
+}
